aa9f3de37dd901ca3c90eb111f67fb923420f57b
Ezequiel Munz <ezequiel.munz@globant.com>
2014-12-29 11:19:56 -0300

    Added Contact classes

---
+++ TabBarExampleApp/TabBarExampleApp/Contacts/ContactsViewController.h
@@ -0,0 +1,15 @@
+//
+//  ContactsViewController.h
+//  VirtualPet
+//
+//  Created by Ezequiel on 12/5/14.
+//  Copyright (c) 2014 Ezequiel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
+#import "ContactCell.h"
+
+@interface ContactsViewController : UIViewController <MFMailComposeViewControllerDelegate, UITableViewDataSource, ContactDelegate>
+
+@end
